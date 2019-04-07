bfda294bdb5e88d29157965bf6c554abcb5cf43e
Toshiaki Maki <tmaki@pivotal.io>
2019-04-07 19:35:53 +0900

    Add create-env.sh

---
+++ config/director/create-env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# http://docs.pivotal.io/platform-automation/v2.1/configuration-management/configure-env.html
+
+set -eo pipefail
+export OM_TARGET=$(terraform output --state=terraform-state/terraform.tfstate ops_manager_dns)
+cat <<EOF > env/env.yml
+target: ${OM_TARGET}
+connect-timeout: 5
+request-timeout: 1800
+skip-ssl-validation: true
+username: ${OM_USERNAME}
+password: ${OM_PASSWORD}
+decryption-passphrase: ${OM_DECRYPTION_PASSPHRASE}
+EOF
