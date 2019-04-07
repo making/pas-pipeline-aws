0cccc10713a0b4046d4dc98f9e83debec9cebf41
Toshiaki Maki <tmaki@pivotal.io>
2019-04-08 00:14:18 +0900

    Update credhub-service-broker/create-vars.sh

--- config/credhub-service-broker/create-vars.sh
+++ config/credhub-service-broker/create-vars.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -eo pipefail
 
-export SINGLETON_AVAILABILITY_ZONE=$(cat terraform-state/terraform.tfstate | jq -r '.modules[0].outputs.azs.value[0]')
-export AVAILABILITY_ZONES=$(cat terraform-state/terraform.tfstate | jq -r '.modules[0].outputs.azs.value | map({name: .})' | tr -d '\n' | tr -d '"')
-export AVAILABILITY_ZONE_NAMES=$(cat terraform-state/terraform.tfstate | jq -r '.modules[0].outputs.azs.value' | tr -d '\n' | tr -d '"')
+export SINGLETON_AVAILABILITY_ZONE=$(terraform output --state=${TF_DIR}/terraform.tfstate --json azs | jq -r '.value[0]')
+export AVAILABILITY_ZONES=$(terraform output --state=${TF_DIR}/terraform.tfstate --json azs | jq -r '.value | map({name: .})' | tr -d '\n' | tr -d '"')
+export AVAILABILITY_ZONE_NAMES=$(terraform output --state=${TF_DIR}/terraform.tfstate azs | tr -d '\n' | tr -d '"')
 export PAS_MAIN_NETWORK_NAME=pas-deployment
 export PAS_SERVICES_NETWORK_NAME=pas-services
 
-cat <<EOF > vars.yml
+cat <<EOF
 availability_zone_names: ${AVAILABILITY_ZONE_NAMES}
 pas_main_network_name: ${PAS_MAIN_NETWORK_NAME}
 pas_services_network_name: ${PAS_SERVICES_NETWORK_NAME}
