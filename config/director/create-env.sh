6175fca817a9fa54335a55d0f3a2c422e0584a17
Toshiaki Maki <tmaki@pivotal.io>
2019-04-07 19:40:12 +0900

    Update create-env.sh

--- config/director/create-env.sh
+++ config/director/create-env.sh
@@ -2,8 +2,8 @@
 # http://docs.pivotal.io/platform-automation/v2.1/configuration-management/configure-env.html
 
 set -eo pipefail
-export OM_TARGET=$(terraform output --state=terraform-state/terraform.tfstate ops_manager_dns)
-cat <<EOF > env/env.yml
+export OM_TARGET=$(terraform output --state=${TERRAFORM_STATE:-terraform-state}/terraform.tfstate ops_manager_dns)
+cat <<EOF
 target: ${OM_TARGET}
 connect-timeout: 5
 request-timeout: 1800
