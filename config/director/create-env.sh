d373aaa03ec987b3e3e4151133a42fc0d1c01bdb
Toshiaki Maki <tmaki@pivotal.io>
2019-04-07 22:45:17 +0900

    Double request-timeout

--- config/director/create-env.sh
+++ config/director/create-env.sh
@@ -6,7 +6,7 @@ export OM_TARGET=$(terraform output --state=${TERRAFORM_STATE:-terraform-state}/
 cat <<EOF
 target: ${OM_TARGET}
 connect-timeout: 5
-request-timeout: 3600
+request-timeout: 7200
 skip-ssl-validation: ${OM_SKIP_SSL_VALIDATION:-true}
 username: ${OM_USERNAME}
 password: ${OM_PASSWORD}
